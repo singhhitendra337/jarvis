28f6dab44e92ed96a06be0018a132303d1a5e7ec
Avdhesh-Varshney <114330097+Avdhesh-Varshney@users.noreply.github.com>
2025-05-18 15:31:24 +0530

    sidebar layout update

--- Jarvis.py
+++ Jarvis.py
@@ -7,38 +7,59 @@ auth_page = st.Page("src/auth/auth.py", title="Authentication", icon=":material/
 home = st.Page("src/apps/public/home.py", title="Home", icon=":material/home:")
 youtubePlaylist = st.Page("src/apps/public/youtubePlaylist.py", title="Jarvis Videos", icon=":material/ondemand_video:")
 
-MAIN_DIR = 'src/apps/pages'
-
-# /automations
-coding = st.Page(f"{MAIN_DIR}/automations/coding.py", title="Coding Platforms", icon=":material/code:")
-websites = st.Page(f"{MAIN_DIR}/automations/website.py", title="Websites", icon=":material/web:")
-socialMediaApps = st.Page(f"{MAIN_DIR}/automations/socialMediaApps.py", title="Social Media Apps", icon=":material/share:")
-messenger = st.Page(f"{MAIN_DIR}/automations/messenger.py", title="Messenger", icon=":material/email:")
-
-# /models
-chatBotModels = st.Page(f"{MAIN_DIR}/models/chatBotModel.py", title="Chat Bot Models", icon=":material/smart_toy:")
-healthCareModels = st.Page(f"{MAIN_DIR}/models/healthCareModel.py", title="Health Care Models", icon=":material/health_and_safety:")
-objectDetectionModels = st.Page(f"{MAIN_DIR}/models/objectDetectionModel.py", title="Object Detection Models", icon=":material/camera_alt:")
-recommendationModels = st.Page(f"{MAIN_DIR}/models/recommendationModel.py", title="Recommendation Models", icon=":material/recommend:")
+MAIN_DIR = "src/apps/pages"
 
 # /programs
 simplePrograms = st.Page(f"{MAIN_DIR}/programs/simpleProgram.py", title="Simple Programs", icon=":material/emoji_objects:")
 apiPrograms = st.Page(f"{MAIN_DIR}/programs/apiProgram.py", title="API Programs", icon=":material/api:")
 imagePrograms = st.Page(f"{MAIN_DIR}/programs/imageProgram.py", title="Image Programs", icon=":material/image:")
-games = st.Page(f"{MAIN_DIR}/programs/games.py",title="Games",icon=":material/casino:")
 studyPrograms = st.Page(f"{MAIN_DIR}/programs/studyProgram.py", title="Study Programs", icon=":material/school:")
+games = st.Page(f"{MAIN_DIR}/programs/games.py",title="Games",icon=":material/casino:")
+
+# /models
+chatBotModels = st.Page(f"{MAIN_DIR}/models/chatBotModel.py", title="Chat Bot Models", icon=":material/smart_toy:")
+healthCareModels = st.Page(f"{MAIN_DIR}/models/healthCareModel.py", title="Health Care Models", icon=":material/health_and_safety:")
+recommendationModels = st.Page(f"{MAIN_DIR}/models/recommendationModel.py", title="Recommendation Models", icon=":material/recommend:")
+objectDetectionModels = st.Page(f"{MAIN_DIR}/models/objectDetectionModel.py", title="Object Detection Models", icon=":material/camera_alt:")
+
+# /automations
+messenger = st.Page(f"{MAIN_DIR}/automations/messenger.py", title="Messenger", icon=":material/email:")
+socialMediaApps = st.Page(f"{MAIN_DIR}/automations/socialMediaApps.py", title="Social Media Apps", icon=":material/share:")
+websites = st.Page(f"{MAIN_DIR}/automations/website.py", title="Websites", icon=":material/web:")
+coding = st.Page(f"{MAIN_DIR}/automations/coding.py", title="Coding Platforms", icon=":material/code:")
 
 def application():
   pages = {
-    "": [home, youtubePlaylist],
-    "Account": [auth_page],
+    "": [
+      home,
+      youtubePlaylist,
+    ],
+    "Account": [
+      auth_page,
+    ],
   }
 
   if st.user and st.user.is_logged_in:
     pages.update({
-      "Automations": [coding, websites, socialMediaApps, messenger],
-      "Models": [chatBotModels, healthCareModels, objectDetectionModels, recommendationModels],
-      "Programs": [apiPrograms, games, imagePrograms, simplePrograms, studyPrograms],
+      "Programs": [
+        simplePrograms,
+        apiPrograms,
+        imagePrograms,
+        studyPrograms,
+        games,
+      ],
+      "Models": [
+        chatBotModels,
+        healthCareModels,
+        recommendationModels,
+        objectDetectionModels,
+      ],
+      "Automations": [
+        messenger,
+        socialMediaApps,
+        websites,
+        coding,
+      ],
     })
 
   return st.navigation(pages)
